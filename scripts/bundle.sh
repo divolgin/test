21610705b672592aea2ab7f1373beeb7656a7da3
divolgin <dmitriy@replicated.com>
2022-05-03 16:06:31 -0700

    get nodes, not describe nodes

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,11 +1,11 @@
-set -e
+set +e
 
 OUTPUTDIR=$(mktemp -d)
 BUNDLENAME=./manual-support-data.tar.gz
 
 kubectl get pods -o wide -A 1>> $OUTPUTDIR/get-pods.log 2>> $OUTPUTDIR/get-pods.err
 kubectl describe pods -A 1>> $OUTPUTDIR/describe-pods.log 2>> $OUTPUTDIR/describe-pods.err
-kubectl describe nodes -o wide -A 1>> $OUTPUTDIR/describe-nodes.log 2>> $OUTPUTDIR/describe-nodes.err
+kubectl get nodes -o wide -A 1>> $OUTPUTDIR/get-nodes.log 2>> $OUTPUTDIR/get-nodes.err
 df -kh 1>> $OUTPUTDIR/df.log 2>> $OUTPUTDIR/df.err
 docker info 1>> $OUTPUTDIR/docker-info.log 2>> $OUTPUTDIR/docker-info.err
 

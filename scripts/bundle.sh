1bd53f537d2ffe2fb7e35f13df05ac399eafe1c6
divolgin <dmitriy@replicated.com>
2022-05-03 16:03:35 -0700

    Add nodes

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -3,8 +3,9 @@ set -e
 OUTPUTDIR=$(mktemp -d)
 BUNDLENAME=./manual-support-data.tar.gz
 
-kubectl get pods -A 1>> $OUTPUTDIR/get-pods.log 2>> $OUTPUTDIR/get-pods.err
+kubectl get pods -o wide -A 1>> $OUTPUTDIR/get-pods.log 2>> $OUTPUTDIR/get-pods.err
 kubectl describe pods -A 1>> $OUTPUTDIR/describe-pods.log 2>> $OUTPUTDIR/describe-pods.err
+kubectl describe nodes -o wide -A 1>> $OUTPUTDIR/describe-nodes.log 2>> $OUTPUTDIR/describe-nodes.err
 df -kh 1>> $OUTPUTDIR/df.log 2>> $OUTPUTDIR/df.err
 docker info 1>> $OUTPUTDIR/docker-info.log 2>> $OUTPUTDIR/docker-info.err
 

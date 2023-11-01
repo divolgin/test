d46ab7de9f57e2c0329de7808d6a42127e851858
divolgin <dmitriy@replicated.com>
2023-11-01 15:15:17 -0700

    iterate run data

--- .github/actions/gate-production-release-workflow/index.js
+++ .github/actions/gate-production-release-workflow/index.js
@@ -1,39 +1,85 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+async function get_workflow_runs() {
+  const token = core.getInput("gh_token", { required: true });
+  const ref = core.getInput("ref", { required: true });
+  const run_id = core.getInput("run_id", { required: true });
+
+  console.log("+++++ref", ref);
+  console.log("+++++run_id", run_id);
+
+  // get the commit SHA from the ref
+  // const sha = await revParse(ref);
+
+  // set up github client
+  const octokit = github.getOctokit(token);
+
+  const {
+    data: {
+      workflow_runs,
+    },
+  } = await octokit.rest.actions.listWorkflowRunsForRepo({
+    ...github.context.repo,
+  });
+
+  console.log("+++++workflow_runs", workflow_runs);
+
+  const this_run = null;
+  for (const workflow_run in workflow_runs) {
+    if (workflow_run.id == run_id) {
+      this_run = workflow_run;
+      break;
+    }
+  }
+
+  if (!this_run) {
+    throw new Error(`Could not find workflow ${run_id} run`);
+  }
+
+  console.log("+++++this run", this_run);
+  const earlier_runs = []; // runs that were triggered earlier and still have not completed
+  const later_runs = []; // runs that were triggered after this one and have not completed
+
+  console.log("Looking for earlier and later runs");
+  for (const workflow_run in workflow_runs) {
+    if (this_run.name !== workflow_run.name || workflow_run.status === "completed") {
+      continue;
+    }
+
+    if (workflow_run.created_at < this_run.created_at) {
+      console.log(`Found earlier run ${workflow_run.html_url}`);
+      earlier_runs.append(workflow_run);
+      continue;
+    }
+
+    if (workflow_run.created_at > this_run.created_at){
+      console.log(`Found later run ${workflow_run.html_url}`);
+      later_runs.append(workflow_run);
+      continue;
+    }
+  }
+
+  return {earlier_runs, later_runs};
+}
+
 async function run() {
-  try {
-    const token = core.getInput("gh_token", { required: true });
-    const ref = core.getInput("ref", { required: true });
-    const run_id = core.getInput("run_id", { required: true });
+  // If there are workflow runs that started earlier, wait for them finish before proceeding.
+  // If there are workflows that started later, stop this workflow. Let new one run.
+  while (true) {
+    const {earlier_runs, later_runs} = await get_workflow_runs();
+    if (earlier_runs.length > 0) {
+      console.log(`Found ${earlier_runs.length} earlier runs. Waiting for them to finish.`)
+      await sleep(5000);
+      continue;
+    }
 
-    console.log("+++++ref", ref);
-    console.log("+++++run_id", run_id);
+    if (later_runs.length > 0) {
+      console.log(`Found ${later_runs.length} later runs that have already started. Stopping this workflow.`)
+      return "stop"
+    }
 
-    // get the commit SHA from the ref
-    // const sha = await revParse(ref);
-
-    // set up github client
-    const octokit = github.getOctokit(token);
-    console.log("+++++octokit", octokit);
-
-    // create new release with release notes (or placeholder)
-    // core.debug(
-    //   `call: octokit.rest.actions.listWorkflowRunsForRepo(${JSON.stringify({
-    //     ...github.context.repo,
-    //   })})`
-    // );
-    const {
-      data,
-    } = await octokit.rest.actions.listWorkflowRunsForRepo({
-      ...github.context.repo,
-    });
-    // core.debug(`return: ${JSON.stringify({ data })}`);
-    console.log("+++++data", data);
-
-  } catch (error) {
-    // core.setFailed(error.message);
-    console.log("+++++error", error);
+    return "trigger"
   }
 }
 
